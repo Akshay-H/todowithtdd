1c96172512c9cdb02aafd651f61896646f842793
Akshay <akshayhari555@gmail.com>
2022-02-06 16:23:49 +0530

    Added tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [currentTodo, setCurrentTodo] = useState("");
+  const [editingTodo, setEditingTodo] = useState("");
+
+  function addTodo() {
+    setTodos([...todos, { title: currentTodo, id: nanoid() }]);
+    setCurrentTodo("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={currentTodo}
+        onChange={(e) => setCurrentTodo(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            addTodo();
+          }
+        }}
+        placeholder="Enter todo item"
+      />
+      <button onClick={() => addTodo()}>Add Todo</button>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <input
+            type="checkbox"
+            data-testid="check-todo"
+            onClick={() => {
+              changeTodoDoneStatus(todo.id);
+            }}
+          />
+          {todo.editing ? (
+            <button
+              onClick={() => {
+                saveTodo();
+              }}
+            >
+              Save
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                setEditStatusOfTodo(todo.id);
+              }}
+            >
+              Edit
+            </button>
+          )}
+
+          <button
+            onClick={() => {
+              const filteredTodos = todos.filter((t) => t.id !== todo.id);
+              setTodos(filteredTodos);
+            }}
+          >
+            Delete
+          </button>
+          {todo.editing ? (
+            <input
+              type="text"
+              value={editingTodo}
+              onChange={(e) => {
+                setEditingTodo(e.target.value);
+              }}
+            />
+          ) : (
+            <span style={{ textDecoration: todo.done ? "line-through" : "" }}>
+              {todo.title}
+            </span>
+          )}
+        </div>
+      ))}
     </div>
   );
+
+  function setEditStatusOfTodo(id) {
+    const editingTodos = todos.map((t) => {
+      t.editing = false;
+      if (t.id === id) {
+        t.editing = true;
+        setEditingTodo(t.title);
+      }
+      return t;
+    });
+    setTodos(editingTodos);
+  }
+
+  function saveTodo() {
+    const editedTodos = todos.map((t) => {
+      if (t.editing) {
+        t.title = editingTodo;
+        setEditingTodo("");
+        t.editing = false;
+      }
+      return t;
+    });
+    setTodos(editedTodos);
+  }
+
+  function changeTodoDoneStatus(id) {
+    const newTodos = todos.map((t) => {
+      if (t.id === id) {
+        t.done = !t.done;
+      }
+      return t;
+    });
+    setTodos(newTodos);
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,59 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Todo App", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test("Add todo item by enter key", () => {
+    const todoInput = screen.getByPlaceholderText(/enter todo item/i);
+    userEvent.type(todoInput, "Wash the dishes{enter}");
+    const todoItem = screen.getByText("Wash the dishes");
+    expect(todoInput).toHaveValue("");
+  });
+  test("Add todo item by add todo button", () => {
+    const todoInput = screen.getByPlaceholderText(/enter todo item/i);
+    userEvent.type(todoInput, "Do the laundry");
+    const addTodoButton = screen.getByText(/add todo/i);
+    userEvent.click(addTodoButton);
+    const todoItem = screen.getByText("Do the laundry");
+    expect(todoInput).toHaveValue("");
+  });
+
+  test("Add todo item and check it as done", () => {
+    const todoInput = screen.getByPlaceholderText(/enter todo item/i);
+    userEvent.type(todoInput, "Wash the dishes{enter}");
+    const todoItem = screen.getByText("Wash the dishes");
+    expect(todoInput).toHaveValue("");
+    const checkbox = screen.getByTestId("check-todo");
+    userEvent.click(checkbox);
+    expect(todoItem).toHaveStyle({ textDecoration: "line-through" });
+  });
+
+  test("Delete todo item", () => {
+    const todoInput = screen.getByPlaceholderText(/enter todo item/i);
+    userEvent.type(todoInput, "Wash the dishes{enter}");
+    const todoItem = screen.getByText("Wash the dishes");
+    expect(todoInput).toHaveValue("");
+    const deleteButton = screen.getByText("Delete");
+    userEvent.click(deleteButton);
+    expect(todoItem).not.toBeInTheDocument();
+  });
+
+  test("Editing todo", () => {
+    const todoInput = screen.getByPlaceholderText(/enter todo item/i);
+    userEvent.type(todoInput, "Wash the dishes{enter}");
+    const todoItem = screen.getByText("Wash the dishes");
+    expect(todoInput).toHaveValue("");
+    const editButton = screen.getByText("Edit");
+    userEvent.click(editButton);
+    const editBox = screen.getByDisplayValue("Wash the dishes");
+    userEvent.clear(editBox);
+    userEvent.type(editBox, "Do the laundry");
+    const saveButton = screen.getByText("Save");
+    userEvent.click(saveButton);
+    const editedTodo = screen.getByText("Do the laundry");
+  });
 });
